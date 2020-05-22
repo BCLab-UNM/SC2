e53be496ed1b65775e26c6198c5968631ceb1ef1
Carter Frost <git@carterfrost.com>
2020-05-21 23:31:59 -0700

    misc/warmerColder.py: creation of ros node for finding volatiles

---
+++ misc/warmerColder.py
@@ -0,0 +1,19 @@
+import rospy
+from std_msgs.msg import String
+from srcp2_msgs import msg
+lastVol = None
+
+def callback(data):
+    global lastVol
+    jitterDecimal = 1
+    if lastVol is None or data.vol_index is not lastVol.vol_index:
+        rospy.loginfo("Found new %s, %f from me", data.vol_type, data.distance_to)
+    elif round(data.distance_to, jitterDecimal) < round(lastVol.distance_to, jitterDecimal):
+        rospy.loginfo("Warmer, %s is %f from me", data.vol_type, data.distance_to)
+    elif round(data.distance_to, jitterDecimal) > round(lastVol.distance_to, jitterDecimal):
+        rospy.loginfo("Colder, %s is %f from me", data.vol_type, data.distance_to)
+    lastVol = data
+
+rospy.init_node('warmerColder')
+rospy.Subscriber("/scout_1/volatile_sensor", msg.VolSensorMsg, callback)
+rospy.spin()
