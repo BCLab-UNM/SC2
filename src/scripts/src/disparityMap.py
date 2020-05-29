d5af52409e78372cb6a232830679f5a9a9f61b48
Antonio-Griego <deano505@unm.edu>
2020-05-29 17:09:46 -0600

    added openCV image generation of disparity map
    next steps involve integrating a proper publisher, and
    incorporating the files properly into the build structure of
    the project

--- src/scripts/src/disparityMap.py
+++ src/scripts/src/disparityMap.py
@@ -4,14 +4,19 @@
 import rospy
 import message_filters
 import cv2
+import numpy as np
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 from cv_bridge import CvBridge
-
+from matplotlib import pyplot as plt
+import time
 
 class DisparityMap(object):
 
 	def __init__(self):
+		self.stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+		self.bridge = CvBridge()
+
 		self.left_camera_subscriber = message_filters.Subscriber('/scout_1/camera/left/image_raw', Image)
 		self.right_camera_subscriber = message_filters.Subscriber('/scout_1/camera/right/image_raw', Image)
 
@@ -22,13 +27,27 @@ class DisparityMap(object):
 		self.synchronizer = message_filters.ApproximateTimeSynchronizer([self.left_camera_subscriber, self.right_camera_subscriber], 10, 0.1, allow_headerless=True)
 		self.synchronizer.registerCallback(self.callback)
 
+
 	def callback(self, left_camera_data, right_camera_data):
 		# left_camera_data and right_camera_data are sensor_msg/Image data types
 
-		# code goes here
-		######
+		# convert image data from image message -> opencv image -> numpy array
+		cv_image_left = self.bridge.imgmsg_to_cv2(left_camera_data, desired_encoding="mono8")
+		np_image_left = np.asarray(cv_image_left)
+	
+		cv_image_right = self.bridge.imgmsg_to_cv2(right_camera_data, desired_encoding="mono8")
+		np_image_right = np.asarray(cv_image_right)
 
-		self.disparity_map_publisher.publish('test')
+		disparity = self.stereo.compute(np_image_left, np_image_right)
+		
+		plt.imshow(np_image_left, 'gray')
+		plt.figure()
+		plt.imshow(np_image_right, 'gray')
+		plt.figure()
+		plt.imshow(disparity, 'gray')
+		plt.show()
+		
+		#self.disparity_map_publisher.publish('test')
 
 
 if __name__ == '__main__':
