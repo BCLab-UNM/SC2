ee7d52134ec6a608ca51f7b6b243c31f16ac6888
Antonio-Griego <deano505@unm.edu>
2020-05-29 16:16:12 -0600

    added skeleton code for building the disparity map node

--- src/scripts/src/disparityMap.py
+++ src/scripts/src/disparityMap.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python
 
+
 import rospy
-#from std_msgs.msg import String
-#from srcp2_msgs import msg
-#lastVol = None
-
-def callback(data):
-	# code goes here
-	return
+import message_filters
+import cv2
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from cv_bridge import CvBridge
 
 
-def play():
-    rospy.init_node('disparityMap')
-    # rospy.Subscriber("/scout_1/volatile_sensor", msg.VolSensorMsg, callback)
-	# need to also create a publisher
-    rospy.spin()
+class DisparityMap(object):
+
+	def __init__(self):
+		self.left_camera_subscriber = message_filters.Subscriber('/scout_1/camera/left/image_raw', Image)
+		self.right_camera_subscriber = message_filters.Subscriber('/scout_1/camera/right/image_raw', Image)
+
+		# need to also create a publisher
+		# this is an example, and needs to be updated
+		self.disparity_map_publisher = rospy.Publisher('/test', String, queue_size=10)
+
+		self.synchronizer = message_filters.ApproximateTimeSynchronizer([self.left_camera_subscriber, self.right_camera_subscriber], 10, 0.1, allow_headerless=True)
+		self.synchronizer.registerCallback(self.callback)
+
+	def callback(self, left_camera_data, right_camera_data):
+		# left_camera_data and right_camera_data are sensor_msg/Image data types
+
+		# code goes here
+		######
+
+		self.disparity_map_publisher.publish('test')
 
 
 if __name__ == '__main__':
-    play()
+	rospy.init_node('disparityMap')
+	DisparityMap()
+	rospy.spin()
+
