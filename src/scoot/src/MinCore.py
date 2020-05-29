d5ae840cfa1815b1f2e1351572451182788b5e88
Carter Frost <carter90@gmail.com>
2020-05-29 01:38:32 -0700

    src/scoot/src/MinCore.py: moved code into main, exec bit and header

--- src/scoot/src/MinCore.py
+++ src/scoot/src/MinCore.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
 import rospy
 from Driver import State
-rospy.init_node('corez')
-r = rospy.Rate(10) # 10hz
-driver = State()
 
-while not rospy.is_shutdown():
-    driver.run()
-    r.sleep()
+    
+if __name__ == '__main__':
+    try:
+        rospy.loginfo("MinCore Starting")
+        rospy.init_node('MinCore')
+        r = rospy.Rate(10) # 10hz
+        driver = State()
+        rospy.loginfo("MinCore Started")
+        while not rospy.is_shutdown():
+            driver.run()
+            r.sleep()
+    except rospy.ROSInterruptException:
+        pass
