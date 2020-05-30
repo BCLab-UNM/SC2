015c6b8963aa2bef7e6b83404f762591702ce538
Carter Frost <carter90@gmail.com>
2020-05-30 02:22:46 -0700

    src/scoot/behaviors/rovertype/behavior.py:  behavior place holders

---
+++ src/scoot/behaviors/excavator/dig.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+"""Dig node."""
+
+from __future__ import print_function
+import sys
+
+def main(**kwargs):
+    print ("Dig Node Started")
+    sys.exit(0)
+
+
+if __name__ == '__main__' : 
+    #scoot.start(node_name='dig')
+    sys.exit(main())
