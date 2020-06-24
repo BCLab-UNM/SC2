0559f591dd68bc2df4c88ba0db647a788c0adc5a
John Ericksen <johncarl81@gmail.com>
2020-06-24 15:17:36 -0600

    Refactored waypoint navigator into its own class

--- src/scoot/src/DDSA.py
+++ src/scoot/src/DDSA.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
-
 import rospy, sys, tf
-import numpy as np
-import angles
-from Scoot import VolatileException, ObstacleException, PathException, AbortException, MoveResult
 from Scoot import *
 from geometry_msgs.msg import Point
 from nav_msgs.msg import Odometry
 from enum import Enum
-
-current_odom = None
+from WaypointNavigator import WaypointNavigator
 
 class Span(Enum):
     WALK = 1
@@ -19,7 +14,7 @@ def createWaypoint(x, y, altitude):
     return Point(x, y, altitude)
 
 def calculateRange(type, x1, y1, x2, y2, length):
-    print "TYPE: {} {} {}".format(type, Span.WALK, Span.RANGE)
+    # Build the range with either a walk (steps between point 1 and 2) or range (go directly to point 2)
     if type == Span.WALK:
         waypoints = []
         print "Calculating walk"
@@ -60,10 +55,7 @@ def buildDDSAWaypoints(rangeType, centerx, centery, altitude, size, index, loops
                 if (corner == 2 or corner == 3):
                     yoffset = -yoffset
 
-            print "{}, {} -> {}, {}".format(previousxoffset, previousyoffset, xoffset, yoffset)
-
             for (x, y) in calculateRange(rangeType, previousxoffset, previousyoffset, xoffset, yoffset, steplength):
-                print "y {}. {}".format(x, y)
                 waypoints.append(buildWaypoint(centerx, centery, x * radius, y * radius, altitude))
 
             previousxoffset = xoffset
@@ -71,85 +63,27 @@ def buildDDSAWaypoints(rangeType, centerx, centery, altitude, size, index, loops
 
     return waypoints
 
-def position(odom):
-    global current_odom
-    current_odom = odom
-
-def get2DPose(odometry):
-    """gets this robot's position"""
-    position = odometry.pose.pose.position
-    rotation = odometry.pose.pose.orientation
-    # Convert to usable angle
-    euler = tf.transformations.euler_from_quaternion(quaternion=(rotation.x, rotation.y, rotation.z, rotation.w))
-    theta = euler[2]
-    if theta < -math.pi:
-        theta = theta + (2*math.pi)
-    elif theta > math.pi:
-        theta = theta - (2*math.pi)
-    return { 'x': position.x, 'y': position.y, 'theta': theta }
-
-def toRadians(degree):
-    return 2 * math.pi * degree / 360.0
-
-def unit_vector(vector):
-    """ Returns the unit vector of the vector.  """
-    return vector / np.linalg.norm(vector)
-
-def angle_between(v1, v2):
-    v1_u = unit_vector(v1)
-    v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
-
-def gotowaypoint(waypoint, scoot):
-    distance = math.hypot(current_odom.pose.pose.position.x - waypoint.x, current_odom.pose.pose.position.y - waypoint.y)
-    while distance > 0.9 :
-        pose = get2DPose(current_odom)
-        poseAngle = pose['theta']
-        # TODO: Remove this when the IMU flip is fixed
-        poseAngle = -poseAngle
-        angleto = math.atan2(current_odom.pose.pose.position.y - waypoint.y, -current_odom.pose.pose.position.x + waypoint.x)
-        angle = angles.shortest_angular_distance(poseAngle, angleto)
-
-        if abs(angle) < toRadians(5):
-            # Move forward
-            scoot.drive(1)
-        elif abs(angle) < toRadians(70):
-            # Soft Turn
-            scoot.turn(toRadians(-10 if angle > 0 else 10))
-        else:
-            # Hard Turn
-            scoot.turn(toRadians(-50 if angle > 0 else 50))
-
-        distance = math.hypot(waypoint.x - current_odom.pose.pose.position.x, waypoint.y - current_odom.pose.pose.position.y)
-    print "Reached waypoint: {}".format(waypoint)
-
-
-
-
-def followDDSA(name):
-
-    global scoot
-
-    ddsaWaypoints = buildDDSAWaypoints(Span.RANGE, 0, 0, 0, 1, 0, 5, 1, 1)
-
-    rospy.Subscriber("{}/odom/filtered".format(name), Odometry, position)
-    rospy.wait_for_message("{}/odom/filtered".format(name), Odometry)
-
-    zero_odom = current_odom
-
-    anglediff = angle_between((1, -1, 0), (1, 1, 0))
-    print (360 * anglediff / (math.pi * 2))
-
-    for waypoint in ddsaWaypoints :
-        print "Going to {}".format(waypoint)
-        gotowaypoint(Point(waypoint.x + zero_odom.pose.pose.position.x, waypoint.y + zero_odom.pose.pose.position.y, 0), scoot)
-
-
-
 if __name__ == '__main__':
-    global scoot
-    rospy.init_node('test')
-    scoot = Scoot("scout_1")
+    rospy.init_node('ddsa_navigator')
+
+    print "DDSA Navigator started."
+
+    name = 'scout_1'
+
+    scoot = Scoot(name)
     scoot.start(node_name='test')
 
-    followDDSA('scout_1')
+    navigator = WaypointNavigator(scoot)
+
+    rospy.Subscriber("{}/odom/filtered".format(name), Odometry, navigator.position)
+    rospy.wait_for_message("{}/odom/filtered".format(name), Odometry)
+
+    ddsaWaypoints = buildDDSAWaypoints(Span.RANGE,
+                                       navigator.getPosition().pose.pose.position.x,
+                                       navigator.getPosition().pose.pose.position.y,
+                                       0, 1, 0, 5, 1, 1)
+
+    print "Waypoints generated, size: {}".format(len(ddsaWaypoints))
+
+    print "Navigating Waypoints..."
+    navigator.navigate(ddsaWaypoints)
---
+++ src/scoot/src/WaypointNavigator.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+import angles, math, tf
+
+def toRadians(degree):
+    return 2 * math.pi * degree / 360.0
+
+def prettyPoint(point):
+    return "({0:0.2f}, {1:0.2f})".format(point.x, point.y)
+
+class WaypointNavigator:
+    def __init__(self, scoot):
+        self.scoot = scoot
+        self.current_odom = None
+
+    def position(self, odom):
+        self.current_odom = odom
+
+    def getPosition(self):
+        return self.current_odom
+
+    def navigate(self, waypoints):
+        for i in range(len(waypoints)) :
+            print "Going to waypoint {}".format(i)
+            self.navigateToWaypoint(waypoints[i])
+
+    def get2DPose(self):
+        rotation = self.current_odom.pose.pose.orientation
+        euler = tf.transformations.euler_from_quaternion(quaternion=(rotation.x, rotation.y, rotation.z, rotation.w))
+        return euler[2]
+
+
+    def navigateToWaypoint(self, waypoint):
+        distance = float("inf")
+        while distance > 0.9 :
+            posePosition = self.current_odom.pose.pose.position
+            poseAngle = self.get2DPose()
+            # TODO: Remove this when the IMU flip is fixed
+            poseAngle = -poseAngle
+            angleto = math.atan2(posePosition.y - waypoint.y, -posePosition.x + waypoint.x)
+            angle = angles.shortest_angular_distance(poseAngle, angleto)
+
+            if abs(angle) < toRadians(5):
+                # Move forward
+                self.scoot.drive(1)
+            elif abs(angle) < toRadians(70):
+                # Soft Turn
+                # TODO: Implement with forward movement
+                self.scoot.turn(toRadians(-10 if angle > 0 else 10))
+            else:
+                # Hard Turn
+                self.scoot.turn(toRadians(-50 if angle > 0 else 50))
+
+            posePosition = self.current_odom.pose.pose.position
+            distance = math.hypot(waypoint.x - posePosition.x, waypoint.y - posePosition.y)
+        print "Reached waypoint: {} at point: {}".format(prettyPoint(waypoint), prettyPoint(posePosition))
