e3bf0f0b1797b9182cdcdb7ec9a1593a9d8ac759
Carter Frost <carter90@gmail.com>
2020-07-11 00:19:23 -0700

    src/scoot/src/behaviors/scout/home_alignment.py: Creation needed round 3

---
+++ src/scoot/src/behaviors/scout/home_alignment.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+"""Home Alignment node."""
+
+from __future__ import print_function
+import sys
+
+
+def main(task=None):
+    print("Home Alignment Node Started")
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    # scoot.start(node_name='home_alignment')
+    sys.exit(main())
