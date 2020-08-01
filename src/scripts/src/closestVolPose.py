0bc66eee392580798eee66911df479c2605643e3
Carter Frost <carter90@gmail.com>
2020-08-01 01:15:53 -0700

    src/scripts/src/closestVolPose.py: created helper script to get the closest Volatile to the scout

---
+++ src/scripts/src/closestVolPose.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from gazebo_msgs.srv import GetModelState
+import math
+
+def main():
+    cur_pose = model_coordinates("scout_1", 'world').pose.position
+    all_vols_poses =[ model_coordinates("vol_"+str(num), "world").pose.position for num in range(0,29) ]
+    closest_vol_pose = min(all_vols_poses, key=lambda k: math.sqrt((k.x - cur_pose.x) ** 2 + (k.y - cur_pose.y) ** 2))
+    print("Rover pose:")
+    print(cur_pose)
+    print("\n\n Closest Volatile pose:")
+    print(closest_vol_pose)
+
+
+if __name__ == '__main__':
+    main()
