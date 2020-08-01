f47be77111be7ab02ef72aa9a23b82cc4171ae4b
Carter Frost <carter90@gmail.com>
2020-08-01 01:21:20 -0700

    src/scripts/src/closestVolPose.py: added rospy import and populate model_coordinates as service proxy

--- src/scripts/src/closestVolPose.py
+++ src/scripts/src/closestVolPose.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from gazebo_msgs.srv import GetModelState
+import rospy
 import math
 
 def main():
+    model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
     cur_pose = model_coordinates("scout_1", 'world').pose.position
     all_vols_poses =[ model_coordinates("vol_"+str(num), "world").pose.position for num in range(0,29) ]
     closest_vol_pose = min(all_vols_poses, key=lambda k: math.sqrt((k.x - cur_pose.x) ** 2 + (k.y - cur_pose.y) ** 2))
