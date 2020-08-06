318c64de74704fde94c250cc209f350a528e7d15
abbypribis <abbypribis@gmail.com>
2020-08-06 16:35:50 -0600

    I want this behavior to just call score and error handling be in score - still needs testing

--- src/scoot/src/behaviors/scout/searchRandomWalk.py
+++ src/scoot/src/behaviors/scout/searchRandomWalk.py
@@ -39,12 +39,9 @@ def random_walk(num_moves):
                 sys.exit(-1)
             wander()
     except VolatileException:
-        rospy.loginfo("I found a volatile! " + scoot.VOL_TYPES[scoot.control_data])
-        result = scoot.score(scoot.control_data)
-        if not result:
-            # need to take this in mind get from the param server volatile_detection_service_delay_range
-            rospy.logwarn("First score attempt failed trying again")
-            scoot.score(scoot.control_data)
+        rospy.logwarn("I found a volatile! " + scoot.VOL_TYPES[scoot.control_data])
+        scoot.score(scoot.control_data)
+        rospy.logwarn('Exiting')
         sys.exit(0)
 
 
