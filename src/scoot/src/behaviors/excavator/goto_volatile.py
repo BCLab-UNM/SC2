873ba0969fb277a2d4fc4607aeb72fec97885632
Carter Frost <carter90@gmail.com>
2020-08-13 03:24:12 -0700

    src/scoot/src/behaviors/excavator/goto_volatile.py: basic outline
    **UNTESTED**

--- src/scoot/src/behaviors/excavator/goto_volatile.py
+++ src/scoot/src/behaviors/excavator/goto_volatile.py
@@ -5,8 +5,7 @@ from __future__ import print_function
 import sys
 
 import rospy
-from Scoot import Scoot
-
+from Scoot import Scoot, Location, VolatileException, ObstacleException
 
 def main(task=None):
     if task:
@@ -15,7 +14,19 @@ def main(task=None):
         scoot = Scoot("excavator")
         scoot.start(node_name='goto_volatile')
     rospy.loginfo('Goto Volatile Started')
-    sys.exit(0)  # "succeeded"
+    # test if I need to do .pose.position or something
+    vol_pose = scoot.get_closest_vol()
+    try:
+        scoot.drive_to(vol_pose)
+    except ObstacleException:
+        pass
+    except VolatileException:
+        sys.exit(0)  # "succeeded" # @TODO: might retest mass as this volatile might be almost exhausted
+    # @TODO: obstacle avoidance calls should live here
+    if scoot.OdomLocation.atGoal(vol_pose, 2.0):
+        sys.exit(0)  # "succeeded"
+    else:
+        sys.exit(-1)  # "failed"
 
 
 if __name__ == '__main__':
