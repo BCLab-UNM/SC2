36fe0fa028e5bcc404ba9a6e42494d1997a00f49
Antonio-Griego <deano505@unm.edu>
2020-08-14 14:10:45 -0600

    added basic blob detections topics to simulation
    using this object detection node we:
    1. filter the left and right camera topics
    2. detect blobs
    3. super-impose blobs on original camera images
    4. publish these blob detections into their own camera topics

--- src/object_detection/src/object_detection.py
+++ src/object_detection/src/object_detection.py
@@ -17,11 +17,24 @@ class ObjectDetection(object):
 	def __init__(self):
 		self.bridge = CvBridge()
 
+		# generate and set the parameters for cv2 simple blob detection
+		self.simple_blob_detector_params = cv2.SimpleBlobDetector_Params()
+		self.simple_blob_detector_params.minThreshold = 1
+		self.simple_blob_detector_params.maxThreshold = 255
+		self.simple_blob_detector_params.filterByArea = True
+		self.simple_blob_detector_params.minArea = 1
+		self.simple_blob_detector_params.filterByCircularity = False
+		self.simple_blob_detector_params.filterByConvexity = False
+		self.simple_blob_detector_params.filterByInertia = False
+
+		# create simple blob detector object
+		self.detector = cv2.SimpleBlobDetector_create(self.simple_blob_detector_params)
+
 		self.left_camera_subscriber = message_filters.Subscriber('/scout_1/camera/left/image_raw', Image)
 		self.right_camera_subscriber = message_filters.Subscriber('/scout_1/camera/right/image_raw', Image)
 
-                # TODO: we need to add a different publisher later
-		# self.disparity_map_publisher = rospy.Publisher('/scout_1/camera/disparity', Image, queue_size=10)
+		self.blob_detection_left_publisher = rospy.Publisher('/scout_1/left/blob_detections', Image, queue_size=10)
+		self.blob_detection_right_publisher = rospy.Publisher('/scout_1/right/blob_detections', Image, queue_size=10)
 
 		self.synchronizer = message_filters.ApproximateTimeSynchronizer([self.left_camera_subscriber, self.right_camera_subscriber], 10, 0.1, allow_headerless=True)
 		self.synchronizer.registerCallback(self.callback)
@@ -32,14 +45,21 @@ class ObjectDetection(object):
 
 		# convert image data from image message -> opencv image -> numpy array
 		cv_image_left = self.bridge.imgmsg_to_cv2(left_camera_data, desired_encoding="mono8")
-		# np_image_left = np.asarray(cv_image_left)	
 		cv_image_right = self.bridge.imgmsg_to_cv2(right_camera_data, desired_encoding="mono8")
-		# np_image_right = np.asarray(cv_image_right)
 
-		# convert our disparity array into a cv2 image and publish
-		# disparity_cv2 = cv2.normalize(src=disparity, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-		# imgmsg = self.bridge.cv2_to_imgmsg(disparity_cv2, encoding="passthrough")
-		# self.disparity_map_publisher.publish(imgmsg)
+		# generate the blob detections
+		keypoints_left = self.detector.detect(cv_image_left)
+		keypoints_right = self.detector.detect(cv_image_right)
+
+		# super-impose the blob detections over the original camera image
+		im_with_keypoints_left = cv2.drawKeypoints(cv_image_left, keypoints_left, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+		im_with_keypoints_right = cv2.drawKeypoints(cv_image_right, keypoints_right, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+		# convert cv2 images into ros messages and publish
+		imgmsg_left = self.bridge.cv2_to_imgmsg(im_with_keypoints_left, encoding="passthrough")
+		imgmsg_right = self.bridge.cv2_to_imgmsg(im_with_keypoints_right, encoding="passthrough")
+		self.blob_detection_left_publisher.publish(imgmsg_left)
+		self.blob_detection_right_publisher.publish(imgmsg_right)
 
 
 if __name__ == '__main__':
