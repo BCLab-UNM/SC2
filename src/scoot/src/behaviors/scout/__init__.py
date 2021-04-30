679f05d7f197967146838055a8572ae04239f3fd
Carter Frost <git@carterfrost.com>
2021-04-30 03:12:47 -0700

    src/scoot/src/behaviors/scout/home_alignment.py: behavior deletion

--- src/scoot/src/behaviors/scout/__init__.py
+++ src/scoot/src/behaviors/scout/__init__.py
@@ -3,7 +3,6 @@ from . import search
 from . import searchRandomWalk
 from . import fine_search
 from . import DDSA
-from . import home_alignment
 from . import goto_waypoint
 from . import goto_processing_plant
 
