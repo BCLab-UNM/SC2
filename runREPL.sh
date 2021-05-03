63671f39d527f045007b572f141e8f922487ebc5
John Ericksen <johncarl81@gmail.com>
2021-05-03 16:12:59 -0600

    Added model name to runREPL.sh script

--- runREPL.sh
+++ runREPL.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+if [[ -z $1 || $1 = S* || $1 = s* ]]
+then
+  name="small_scout_1"
+elif [[ $1 = E* || $1 = e* ]]
+then
+  name="small_excavator_1"
+elif [[ $1 = H* || $1 = h* ]]
+then
+  name="small_hauler_1"
+else
+  echo "Unable to start command with unmapped name $1"
+  exit 1
+fi
+
+echo "Starting docker REPL with $name"
+
 docker run -it \
     --network srcp2net \
     -v /SC2:$(pwd) \
     swarmathon:development \
-    /bin/sh -c 'roslaunch scoot scoot.launch "mode:=man" &
+    /bin/sh -c "roslaunch scoot scoot.launch \"mode:=man\" \"name:=$name\" &
                 sleep 2 &&
-                ROS_NAMESPACE="/small_scout_1" rosrun scoot repl.py'
+                ROS_NAMESPACE=\"/$name\" rosrun scoot repl.py"
