17a3a2cb6ffae23c17c608db78f2d803bfb3cdd8
Matthew Fricke <matthew@fricke.co.uk>
2021-05-21 16:43:15 -0600

    Handle Wheel pose in degrees and IMU offset For some reason the pose handler for the wheels is in degrees so had to add a conversion to radians (this must be new) The IMU's yaw is offset from the rover by 45 degrees
    Had to add a factor of 2pi. I can see no reason why this was needed but results in a usable wheel encoder.

--- src/localisation/src/wheel_encoder.py
+++ src/localisation/src/wheel_encoder.py
@@ -2,6 +2,7 @@
 
 # Generates odometry from wheel poses 
 
+import math 
 import sys
 import tf.transformations as transform
 from sensor_msgs.msg import JointState, Imu
@@ -38,6 +39,7 @@ class WheelEncoder:
         # self.track_width = 1.75 # meters
         self.track_width = 1 # meters
         self.sample_rate = 1
+        self.msg_min_delta = 0.1 # seconds <--- Changing this from 0.1 spoils the accuracy. That should not be the case!
         self.message_count = 0
 
     def imuCallback(self, data):
@@ -47,7 +49,7 @@ class WheelEncoder:
             data.orientation.z,
             data.orientation.w)
         roll, pitch, yaw = transform.euler_from_quaternion(q) # in [-pi, pi]
-        self.theta = yaw
+        self.theta = yaw + 0.78 # rotate 45 degrees 
 
         
     def jointStatesCallback(self, data):
@@ -57,17 +59,18 @@ class WheelEncoder:
 
         if self.message_count % self.sample_rate != 0:
             return
-        
-        back_left_wheel_angle = data.position[ data.name.index('bl_wheel_joint') ]
-        back_right_wheel_angle = data.position[ data.name.index('br_wheel_joint') ]
-        front_left_wheel_angle = data.position[ data.name.index('fl_wheel_joint') ]
-        front_right_wheel_angle = data.position[ data.name.index('fr_wheel_joint') ]
+
+        # Divide by 180 to get radians
+        back_left_wheel_angle = data.position[ data.name.index('bl_wheel_joint') ]*math.pi/180
+        back_right_wheel_angle = data.position[ data.name.index('br_wheel_joint') ]*math.pi/180
+        front_left_wheel_angle = data.position[ data.name.index('fl_wheel_joint') ]*math.pi/180
+        front_right_wheel_angle = data.position[ data.name.index('fr_wheel_joint') ]*math.pi/180
         
         current_time = rospy.Time.now()  
         dt = (current_time - self.last_time).to_sec()
 
         # Check that sufficient time has passed to avoid duplicate time stamps
-        if dt < 1: # Requre at least 1 second to have passed between function execution
+        if dt < self.msg_min_delta: # Specify the minimum time between messages for function execution (to prevent timestamp warning killing the repl)
             return
         
         ### Calculate angular velocity ###
@@ -102,10 +105,16 @@ class WheelEncoder:
         v_left = omega_left * self.wheel_radius
         v_right = omega_right * self.wheel_radius
 
-        v_rx = ( v_right + v_left ) /2
+        print("Wheel Radius: ", self.wheel_radius)
+
+        v_rx = ( v_right + v_left ) / 2.0
         v_ry = 0
+
+        print("Velocity: ", v_rx, " m/s") 
+        print("Yaw: ", self.theta)
+        
         # We increase the track width by a factor of 4 to match empirical tests
-        v_rtheta = ( v_right - v_left ) / self.track_width
+        v_rtheta = ( v_right - v_left ) / 2*self.track_width
         
         # Velocities and pose in the odom frame
         # The velocities expressed in the robot base frame can be transformed into the odom frame. 
@@ -113,15 +122,26 @@ class WheelEncoder:
         # We are operating in the x-y plane, we ignore altitude - which will result in errors as the robot climbs up and down.
         # Integrate the velocities over time (multiply by the time differential dt and sum)
 
-        v_wx = (v_rx * cos(self.theta) - v_ry * sin(self.theta)) * dt
-        v_wy = (v_rx * sin(self.theta) + v_ry * cos(self.theta)) * dt
-        #v_wx = v_rx*cos(self.theta+v_rtheta/2)
-        #v_wy = v_rx*sin(self.theta+v_rtheta/2)
+        #v_wx = (v_rx * cos(self.theta) - v_ry * sin(self.theta)) * dt
+        #v_wy = (v_rx * sin(self.theta) + v_ry * cos(self.theta)) * dt
 
-        v_wtheta = v_rtheta * dt 
+        # We know our heading from the imu - no need to calculate it from wheel velocities
+        v_wx = v_rx*cos(self.theta)
+        v_wy = v_rx*sin(self.theta)
+
+        print("X displacement: ", v_wx, " m")
+        print("Y displacement: ", v_wy, " m")
+        print("Delta_t: ", dt, " s")
+
+        print("Back Left Wheel Angle: ", back_left_wheel_angle)
+        
+        #v_wtheta = v_rtheta * dt 
                 
-        self.x += v_wx
-        self.y += v_wy
+        self.x += v_wx*2*math.pi # I don't understand why this 2pi factor is needed
+        self.y += v_wy*2*math.pi
+
+        print("X coord: ", self.x)
+        print("Y coord: ", self.y)
 
         # Replaced with IMU theta
         #self.theta += v_wtheta
@@ -176,7 +196,7 @@ class WheelEncoder:
         self.previous_front_left_wheel_angle = front_left_wheel_angle                                
         self.previous_front_right_wheel_angle = front_right_wheel_angle
         self.previous_back_left_wheel_angle = back_left_wheel_angle
-        self.previous_back_right_wheel_angle = back_right_wheel_angle                                
+        self.previous_back_right_wheel_angle = back_right_wheel_angle
         
 def shutdownHandler():
     print("Wheel encoder shutting down.")
