ff32e163f4bdbd97aa34512d3a27b42eee4c47d9
Carter Frost <git@carterfrost.com>
2021-07-01 17:00:25 -0700

    src/behaviors/excavator/goto_volatile.py: +check if has volatile point

--- src/scoot/src/behaviors/excavator/goto_volatile.py
+++ src/scoot/src/behaviors/excavator/goto_volatile.py
@@ -15,18 +15,21 @@ def main(task=None):
         scoot = Scoot("excavator")
         scoot.start(node_name='goto_volatile')
     rospy.loginfo('Goto Volatile Started')
-    vol_pose = scoot.get_closest_vol_pose()
-    try:
-        scoot.drive_to(vol_pose)
-    except ObstacleException:
-        pass
-    except VolatileException:
-        sys.exit(0)  # "succeeded" # @TODO: might retest mass as this volatile might be almost exhausted
-    # @TODO: obstacle avoidance calls should live here
-    if scoot.OdomLocation.at_goal(vol_pose, 2.0):
-        sys.exit(0)  # "succeeded"
-    else:
-        sys.exit(-1)  # "failed"
+    vol_pose = scoot.get_closest_vol_point()
+    # @TODO make a while loop or change state meh
+    if vol_pose:
+        try:
+            scoot.drive_to(vol_pose)
+        except ObstacleException:
+            pass
+        except VolatileException:
+            sys.exit(0)  # "succeeded" # @TODO: might retest mass as this volatile might be almost exhausted
+        # @TODO: obstacle avoidance calls should live here
+        if scoot.OdomLocation.at_goal(vol_pose, 2.0):
+            sys.exit(0)  # "succeeded"
+    else: # no volitile
+        rospy.sleep(5)
+    sys.exit(-1)  # "failed"
 
 
 if __name__ == '__main__':
